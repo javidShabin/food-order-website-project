1eff1b3e77d742d6835b19188c262b52b67d654c
javid shabin <javid.main.act@gmail.com>
2024-09-02 18:31:20 +0530

    Create user login function. rend token as cookie when user logged in

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const { User } = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../utils/token");
 
-// user registration..
+// User registration..
 const registerUser = async (req, res) => {
   try {
     // Get userdata from request body
@@ -25,9 +25,13 @@ const registerUser = async (req, res) => {
     const newUser = new User({ email, ...rest, password: hashedPassword });
     await newUser.save();
 
-    const token = generateToken({_id: newUser.id, email: newUser.email}); // generate token
+    const token = generateToken({ _id: newUser.id, email: newUser.email }); // generate token
     // pass token as cooki the token will expire in one hour
-    res.cookie("token", token, {httpOnly: true, secure: process.env.ENVIRONMENT === "development" ? false : true, maxAge: 1 * 60 * 60 * 1000 });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.ENVIRONMENT === "development" ? false : true,
+      maxAge: 1 * 60 * 60 * 1000,
+    });
     res.json({
       success: true,
       message: "Create new user",
@@ -37,7 +41,51 @@ const registerUser = async (req, res) => {
     console.log(404).json({ error });
   }
 };
+// User login
+const loginUser = async (req, res) => {
+  try {
+    // destructuring fields
+    const { email, password } = req.body;
+    // check if required fields are present
+    if ((!email, !password)) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    // check the user signed or not
+    const isUserExist = await User.findOne({ email });
+    if (!isUserExist) {
+      return res.status(401).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+    // compare password for login
+    const passwordMatch = bcrypt.compareSync(password, isUserExist.password);
+    if (!passwordMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Unatherised access",
+      });
+    }
+    // generate token
+    const token = generateToken(isUserExist._id); // generate token
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.ENVIRONMENT === "development" ? false : true,
+        maxAge: 1 * 60 * 60 * 1000,
+      }); // pass the token as cookie
+    res.json({
+      success: true,
+      message: "User logged in",
+    });
+  } catch (error) {
+    res.status(404).json({ message: "faild to user login" });
+  }
+};
 
 module.exports = {
   registerUser,
+  loginUser,
 };
--- routes/v1/userRouters.js
+++ routes/v1/userRouters.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { registerUser } = require("../../controllers/userController");
+const { registerUser, loginUser } = require("../../controllers/userController");
 const router = express.Router();
 
 // Register a new user
 router.post('/register', registerUser);
 // Login user and get token
-router.post('/login', );
+router.post('/login', loginUser);
 // logout user and clear the token
 router.post('/logout', );
 // get all useres list
