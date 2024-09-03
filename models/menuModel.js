9a95c3f8f86c172d57f67ab4ce6cb4f4d056fb8a
javid shabin <javid.main.act@gmail.com>
2024-09-04 02:36:55 +0530

    changed

--- models/menuModel.js
+++ models/menuModel.js
@@ -1,22 +1,10 @@
 const mongoose = require("mongoose");
 
 const menuSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String, // URL to the image
-    required: false,
-  },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: false },
 });
-const Menu = mongoose.model("menu", menuSchema)
-module.exports = {Menu}
+const Menu = mongoose.model("menu", menuSchema);
+module.exports = { Menu };
