25612c1eb708e8ec9212806ff0a69b88529fd08c
javid shabin <javid.main.act@gmail.com>
2024-09-04 02:13:23 +0530

    Create function for get menu item by id

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -11,31 +11,51 @@ const getMenuItems = async (req, res) => {
   }
 };
 
+// get menu item by id
+const getMenuItemById = async (req, res) => {
+  try {
+    // get menu item id from req.param
+    const { id } = req.params;
+    // find item with id
+    const item = await Menu.findOne({ _id: id });
+    // check if the item have it or not
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+    // if have the item
+    res.status(200).json(item);
+  } catch (error) {
+    res.status(500).json({ message: "error fetching item", error });
+  }
+};
 // create menu items
 const createMenuItem = async (req, res) => {
-  const { name, ...rest } = req.body;
-  if (!name || Object.keys(rest).length === 0) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  const existItem = await Menu.findOne({ name });
-  if (existItem) {
-    return res.status(409).json({ message: "Item already exists" });
-  }
-  if (req.file) {
-    console.log("Uploading file to Cloudinary...");
-    uploadResult = await cloudinaryInstance.uploader.upload(req.file.path)
-    console.log("Upload result:", uploadResult);
-  }
-  const newItem = new Menu({
-    name,
-    ...rest,
-    image: uploadResult.secure_url || "",
-  })
-  const saveMenuItem = await newItem.save()
-  res.status(201).json(saveMenuItem)
+  try {
+    const { name, ...rest } = req.body;
+    if (!name || Object.keys(rest).length === 0) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const existItem = await Menu.findOne({ name });
+    if (existItem) {
+      return res.status(409).json({ message: "Item already exists" });
+    }
+    if (req.file) {
+      console.log("Uploading file to Cloudinary...");
+      uploadResult = await cloudinaryInstance.uploader.upload(req.file.path);
+      console.log("Upload result:", uploadResult);
+    }
+    const newItem = new Menu({
+      name,
+      ...rest,
+      image: uploadResult.secure_url || "",
+    });
+    const saveMenuItem = await newItem.save();
+    res.status(201).json(saveMenuItem);
+  } catch (error) {}
 };
 
 module.exports = {
   getMenuItems,
   createMenuItem,
+  getMenuItemById
 };
--- routes/v1/menuRouters.js
+++ routes/v1/menuRouters.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getMenuItems, createMenuItem } = require('../../controllers/menuController');
+const { getMenuItems, createMenuItem, getMenuItemById } = require('../../controllers/menuController');
 const { upload } = require('../../middlewares/multer');
 const { adminAuthentication } = require('../../middlewares/adminAuth');
 const router = express.Router()
 
 // Get all menu items for a restaurant
 router.get('/allmenus', getMenuItems);
+// get menu by id
+router.get('/item/:id', getMenuItemById)
 // Create menu item
 router.post('/allmenus', adminAuthentication, upload.single("image"), createMenuItem)
 
