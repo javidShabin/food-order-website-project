7694ea345f823d77415c4e0f137ad03d0c5226f4
javid shabin <javid.main.act@gmail.com>
2024-09-03 11:52:10 +0530

    Create route and controller file for admin

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { userRouter } = require("./userRouters");
 const { restRouter } = require("./restRouters");
+const { adminRouter } = require("./adminRouters");
 const v1Router = express.Router();
 
 // user router
 v1Router.use('/user', userRouter)
 // resturant router
 v1Router.use('/restaurant', restRouter)
+// admin rourt
+v1Router.use('/admin', adminRouter)
 
 module.exports = { v1Router };
