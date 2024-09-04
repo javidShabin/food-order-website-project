56b07cbd55bd7eeafd22077cd63686e0a758282e
javid shabin <javid.main.act@gmail.com>
2024-09-05 00:59:31 +0530

    add remove cart item function

--- routes/v1/cartRouters.js
+++ routes/v1/cartRouters.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { addItemToCart, getCart } = require('../../controllers/cartController')
+const { addItemToCart, getCart, removeFromCart } = require('../../controllers/cartController')
 const { userAuthentication } = require('../../middlewares/userAuth')
 const router = express.Router()
 
@@ -8,6 +8,6 @@ router.post('/addCart',userAuthentication, addItemToCart)
 // get cart
 router.get('/getCart',userAuthentication, getCart)
 // remove cart
-router.delete('/remove', )
+router.delete('/remove', userAuthentication, removeFromCart)
 
 module.exports = {cartRouter: router}
