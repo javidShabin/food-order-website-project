c1a03aa7c7133c97714ea90cc6f18bc0dd79ef16
javid shabin <javid.main.act@gmail.com>
2024-09-07 02:05:12 +0530

    Add orging and credential in cors middleware

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const port = 4000;
 
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser())
 
