5d909d9c61c42b89bfa9eb1bcdc4cb3f96533fba
javid shabin <javid.main.act@gmail.com>
2024-09-09 17:03:39 +0530

    Create seller authenticaton and add the othentication when create menu item

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -30,19 +30,31 @@ const getMenuItemById = async (req, res) => {
 // create menu items
 const createMenuItem = async (req, res) => {
   try {
+    const seller = req.seller;
+    console.log(seller, "===seller");
+    // destructure data
     const { name, ...rest } = req.body;
+    // check if required fileds present
     if (!name || Object.keys(rest).length === 0) {
       return res.status(400).json({ message: "All fields are required" });
     }
-    const existItem = await Menu.findOne({ name });
-    if (existItem) {
+
+    // Check if have any same menu item
+    const existMenuItem = await Menu.findOne({ name });
+    if (existMenuItem) {
       return res.status(409).json({ message: "Item already exists" });
     }
+
+    // add image file clodinery and get from cloudinery
     if (req.file) {
       console.log("Uploading file to Cloudinary...");
       uploadResult = await cloudinaryInstance.uploader.upload(req.file.path);
       console.log("Upload result:", uploadResult);
+    } else {
+      console.log("No file to upload.");
     }
+
+    // Save menu data to database
     const newItem = new Menu({
       name,
       ...rest,
@@ -50,7 +62,9 @@ const createMenuItem = async (req, res) => {
     });
     const saveMenuItem = await newItem.save();
     res.status(201).json(saveMenuItem);
-  } catch (error) {}
+  } catch (error) {
+    res.status(404).json({ error });
+  }
 };
 // update menu
 const updateMenu = async (req, res) => {
@@ -74,21 +88,21 @@ const updateMenu = async (req, res) => {
 };
 // delete menu item
 const deleteMenuItem = async (req, res) => {
-    try {
-        const deletedRestaurant = await Menu.findByIdAndDelete(req.params.id)
-        if (!deletedRestaurant) {
-            return res.status(404).json({ message: "Menu item not found" });
-        }
-        res.status(200).json({ message: "Item deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "Error deleting restaurant", error });
+  try {
+    const deletedRestaurant = await Menu.findByIdAndDelete(req.params.id);
+    if (!deletedRestaurant) {
+      return res.status(404).json({ message: "Menu item not found" });
     }
-}
+    res.status(200).json({ message: "Item deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting restaurant", error });
+  }
+};
 
 module.exports = {
   getMenuItems,
   createMenuItem,
   getMenuItemById,
   updateMenu,
-  deleteMenuItem
+  deleteMenuItem,
 };
--- routes/v1/menuRouters.js
+++ routes/v1/menuRouters.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const { getMenuItems, createMenuItem, getMenuItemById, updateMenu, deleteMenuItem } = require('../../controllers/menuController');
-const { upload } = require('../../middlewares/multer');
-const { adminAuthentication } = require('../../middlewares/adminAuth');
-const router = express.Router()
+const express = require("express");
+const {
+  getMenuItems,
+  createMenuItem,
+  getMenuItemById,
+  updateMenu,
+  deleteMenuItem,
+} = require("../../controllers/menuController");
+const { upload } = require("../../middlewares/multer");
+const { sellerAuthentication } = require("../../middlewares/sellerAuth");
+const router = express.Router();
 
 // Get all menu items for a restaurant
-router.get('/allmenus', getMenuItems);
+router.get("/allmenus", getMenuItems);
 // Get menu by id
-router.get('/item/:id', getMenuItemById)
+router.get("/item/:id", getMenuItemById);
 // Create menu item
-router.post('/allmenus', adminAuthentication, upload.single("image"), createMenuItem)
+router.post("/allmenus", sellerAuthentication, upload.single("image"), createMenuItem);
 // Update menus
-router.put('/item/:id', adminAuthentication, updateMenu)
+router.put("/item/:id", sellerAuthentication, updateMenu);
 // Update menus
-router.delete('/item/:id', adminAuthentication, deleteMenuItem)
+router.delete("/item/:id", sellerAuthentication, deleteMenuItem);
 
-module.exports = {menusRouter: router}
+module.exports = { menusRouter: router };
