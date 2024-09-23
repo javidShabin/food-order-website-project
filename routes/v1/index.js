6f70013a43ffeed089aca127e3ab0acac2a51a3d
javid shabin <javid.main.act@gmail.com>
2024-09-23 19:14:57 +0530

    Create payment integration in backend using stripe

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -8,6 +8,7 @@ const { cartRouter } = require("./cartRouters");
 const { reviewRouter } = require("./reviewRouters");
 const { addressRouter } = require("./addressRouter");
 const { sellerRouter } = require("./sellerRouter");
+const {paymentRouter} = require("./paymentRouter");
 const v1Router = express.Router();
 
 // User router
@@ -28,5 +29,7 @@ v1Router.use('/cart', cartRouter)
 v1Router.use('/review', reviewRouter)
 // Address router
 v1Router.use('/addresses', addressRouter)
+// Payment router
+v1Router.use('/payment', paymentRouter)
 
 module.exports = { v1Router };
